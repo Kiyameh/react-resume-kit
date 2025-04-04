0541d54c3303ba66a7347c7837f8772fc871236b
kiyameh <kiyameh@outlook.com>
2025-04-04 20:06:56 +0200

    Corregida base url de vite config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-simple-resume/',
+  base: '/react-resume-kit/',
   build: {
     rollupOptions: {
       external: ['react', 'react/jsx-runtime', 'react-dom', 'react-dom/client'],
