9f8c59dbf7dee3421f5c07d1dbb7dbeabb7155fa
kiyameh <kiyameh@outlook.com>
2025-04-04 21:41:08 +0200

    Eliminadas exportaciones externas

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,14 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react-resume-kit/',
-  build: {
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        '@icons-pack/react-simple-icons',
-        'lucide-react',
-      ],
-    },
-  },
 })
